c490b351877480cea1e658a23e55ab29b821d7a1
GauriPawar3102 <91465282+GauriPawar3102@users.noreply.github.com>
2024-03-18 17:22:47 +0530

    file updated

--- Llama1.py
+++ Llama1.py
@@ -1,9 +1,9 @@
 nbest_size=0
-if (nbest_size==0 || nbest_size=1)
+if (nbest_size==0 || nbest_size=1):
   print("No sampling can be performed")
-else if (nbest_size>1)
+else if (nbest_size>1):
   print("samples from ", nbest_size, " results")
-else 
+else:
   print("use of forward filtering and backward sampling algorithm")
 token_id[5]={'0.1s_rms', '0.3s_ms', '0.5s_sin', '0.7s_cos', '0.9s_tan'}
 
@@ -90,6 +90,106 @@ sequences = pipeline(
 )
 for seq in sequences:
     print(f"Result: {seq['generated_text']}")
+
+if script_args.load_in_8bit and script_args.load_in_4bit:
+    raise ValueError("You can't load the model in 8 bits and 4 bits at the same time")
+elif script_args.load_in_8bit or script_args.load_in_4bit:
+    quantization_config = BitsAndBytesConfig(
+        load_in_8bit=script_args.load_in_8bit, load_in_4bit=script_args.load_in_4bit
+    )
+    device_map = {"": 0}
+    torch_dtype = torch.bfloat16
+else:
+    device_map = None
+    quantization_config = None
+    torch_dtype = None
+
+model = AutoModelForCausalLM.from_pretrained(
+    script_args.model_name,
+    quantization_config=quantization_config,
+    device_map=device_map,
+    trust_remote_code=script_args.trust_remote_code,
+    torch_dtype=torch_dtype,
+    use_auth_token=script_args.use_auth_token,
+)
+
+while(true):
+  print("LLama2 successfully implemented")
+
+from transformers import AutoModelForCasualLM, AutoTokenizer
+model_name="ProsusAI/finbert"
+prompt="What is SAP? and Tell me about architecture of SAP."
+access_token="	04c231ff252c4b5ed3e277120b1cc961b97be14d81825c51c44ba66b4ee8033e"
+
+model=AutoModelForCasualLM.from_pretrained(model_name, device_map="auto", load_in_4bit=True, user_auth_token=access_token)
+Tokenizer=AutoTokenizer.from_pretrained(model_name, use_fast=True, user_auth_token=access_token)
+model_inputs=tokenizer(prompt,return_tensors="pt").to("cuda:0")
+
+output=model_generate(**model_inputs)
+
+print(tokenizer(decode(output[0], skip_special_tokens=True)
+
+hidden_state=0
+def hidden_function(hidden_state)
+  hid_state=hidden_state
+  hid_state=hid_state+1
+
+print(hid_state)
+
+path="https://github.com/GauriPawar3102/MCQgenerator/edit/main/Llama1.py"
+
+from transformers import AutoTokenizer, FlaxLlamaModel
+
+tokenizer = AutoTokenizer.from_pretrained("afmck/testing-llama-tiny")
+model = FlaxLlamaModel.from_pretrained("afmck/testing-llama-tiny")
+inputs = tokenizer("How to implement a note in SAP?", return_tensors="jax")
+outputs = model(**inputs)
+
+last_hidden_states = outputs.last_hidden_state
+
+print(last_hidden_states)
+
+from transformers import AutoTokenizer, FlaxLlamaForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("afmck/testing-llama-tiny")
+model = FlaxLlamaForCausalLM.from_pretrained("afmck/testing-llama-tiny")
+
+inputs = tokenizer("Hello, my dog is cute", return_tensors="np")
+outputs = model(**inputs)
+
+# retrieve logts for next token
+next_token_logits = outputs.logits[:, -1]
+num_key_value_heads=1
+for i in range(1, 3):
+  num_key_value_heads=i
+  if(i==1):
+    print("Model uses Multi Query Attention")
+  else:
+    print("Model uses Group Query Attention")
+  print(next_token_logits[i],"/n")    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
