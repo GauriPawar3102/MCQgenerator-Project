d278be379977c50b192d9514b7b5390711f56d05
GauriPawar3102 <91465282+GauriPawar3102@users.noreply.github.com>
2024-03-18 11:44:57 +0530

    Update Llama1.py
    file updated

--- Llama1.py
+++ Llama1.py
@@ -9,3 +9,6 @@ token_id[5]={'0.1s_rms', '0.3s_ms', '0.5s_sin', '0.7s_cos', '0.9s_tan'}
 
 for i in range(0,5):
   print(i+1, "token id is: ",token_id[i])
+
+my_dict={1: "subparams", 2:"analyzer", 3:"varinats", 4: "words", 5:"mutation" , 6:"patterns", 7:"replacements"}
+  print(my_dict)
