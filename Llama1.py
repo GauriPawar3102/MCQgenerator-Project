30914472255da8f23c27bbc4725c9d34b003f3c9
GauriPawar3102 <91465282+GauriPawar3102@users.noreply.github.com>
2024-03-18 12:59:21 +0530

    file updated

--- Llama1.py
+++ Llama1.py
@@ -12,3 +12,93 @@ for i in range(0,5):
 
 my_dict={1: "subparams", 2:"analyzer", 3:"varinats", 4: "words", 5:"mutation" , 6:"patterns", 7:"replacements"}
   print(my_dict)
+
+#creating cache instance
+from cachetools import cached, TTLCache
+
+cache=TTLcache(max_size=100, ttl=60)
+
+@cached(cache)
+def expensive_function(r=4):
+  int result=r
+  result=result^3
+return result
+
+#creating tuple of tuples
+my_tuple=((batch_size, num_heads, sequence_length, embed_size_per_head))
+  print(my_tuple)
+
+def loss_function():
+  loss=(result*100)/4
+  print(loss)
+  
+from transformers import AutoTokenizer, LlamaForCausalLM
+
+model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+
+prompt = "Hey, are you conscious? Can you talk to me?"
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Generate
+generate_ids = model.generate(inputs.input_ids, max_length=30)
+tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+
+my_token=1
+for i in range(0,100):
+  my_token++
+  my_token=my_token+i
+print(my_token)
+
+
+def inputs_embeds(torch.FloatTensor of shape (batch_size, sequence_length, hidden_size):
+  torch.batch_size=batch_size
+  torch. sequence_length= sequence_length
+  torch.hidden_size=hidden_size
+
+  print(torch.batch_size)
+  print(torch. sequence_length)
+  print(torch.hidden_size)
+
+import numpy as np
+jax.np.dtype=sys(type)
+jax.np.float16=torch.batch_size
+jax.np.float32=torch. sequence_length
+jax.np.bfloat16=torch.hidden_size
+
+from transformers import AutoTokenizer
+import transformers
+import torch
+
+model = "meta-llama/Llama-2-7b-chat-hf"
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float32,
+    device_map="auto",
+)
+
+sequences = pipeline(
+    'what is SAP, tell me more about TMS in SAP',
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    max_length=200,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
+
+
+
+
+
+
+
+
+
+
+
+
