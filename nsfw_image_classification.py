94534365ed74727c0d9289c2d2590310b097fe91
GauriPawar3102 <91465282+GauriPawar3102@users.noreply.github.com>
2024-03-20 10:05:56 +0530

    file created

---
+++ nsfw_image_classification.py
@@ -0,0 +1,23 @@
+# Use a pipeline as a high-level helper
+from PIL import Image
+from transformers import pipeline
+
+img = Image.open("C:\Users\AXW2KOR\Pictures\Tulips.jpg")
+classifier = pipeline("image-classification", model="Falconsai/nsfw_image_detection")
+classifier(img)
+
+#Loading of model directly
+import torch
+from PIL import Image
+from transformers import AutoModelForImageClassification, ViTImageProcessor
+
+img = Image.open("C:\Users\AXW2KOR\Pictures\Tulips.jpg")
+model = AutoModelForImageClassification.from_pretrained("Falconsai/nsfw_image_detection")
+processor = ViTImageProcessor.from_pretrained('Falconsai/nsfw_image_detection')
+with torch.no_grad():
+    inputs = processor(images=img, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+
+predicted_label = logits.argmax(-1).item()
+model.config.id2label[predicted_label]
